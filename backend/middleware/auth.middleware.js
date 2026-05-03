9c3bb91cb17768c0315ca3597e1529e3315e722f
Denislav Volin-Danylov <netranner22845@gmail.com>
2026-05-03 16:53:18 +0300

    change role_name to role_id in auth middleware

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -3,27 +3,23 @@ import jwt from 'jsonwebtoken'
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export const authMiddleware = (req, res, next) => {
-
     const token = req.cookies.token;
-
     if (!token) {
-        return res.status(401).json({ error: 'Токену немає' });
+        return res.status(401).json({ error: 'Не авторизований' });
     }
-
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
-        res.clearCookie('token')
-        return res.status(401).json({ error: 'Некоректний токен' });
+        res.clearCookie('token');
+        return res.status(401).json({ error: 'Недійсний токен' });
     }
-
-}
+};
 
 export const adminMiddleware = (req, res, next) => {
-    if (!req.user || req.user.role !== 'Адміністратор') {
-        return res.status(403).json({ error: 'Недостатньо прав' });
+    if (!req.user || req.user.role_id !== 2) {
+        return res.status(403).json({ error: 'Немає прав доступу' });
     }
     next();
 };
